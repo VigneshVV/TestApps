8602cdb7429d88086a818cb688b56a52d0753a2d
Vignesh VV <vignesh0177@gmail.com>
2017-07-03 21:13:10 +0530

    Scheme shared

--- MyJenkinsTestApp/ViewController.m
+++ MyJenkinsTestApp/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor redColor]];
     [self.view setBackgroundColor:[UIColor redColor]];
+    [self.view setBackgroundColor:[UIColor redColor]];
 
     // Do any additional setup after loading the view, typically from a nib.
 }
